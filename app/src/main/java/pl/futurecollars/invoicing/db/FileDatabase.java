de807777ac6896829d02827326f8e630b97b7405
kstrzelecki <strzelecki@nonstop.co>
2023-02-27 19:14:25 +0100

    profiles .yml

--- app/src/main/java/pl/futurecollars/invoicing/db/FileDatabase.java
+++ app/src/main/java/pl/futurecollars/invoicing/db/FileDatabase.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Repository;
 import pl.futurecollars.invoicing.configuration.InvoicingAppConfiguration;
 import pl.futurecollars.invoicing.model.Invoice;
 import pl.futurecollars.invoicing.service.FileService;
 
+@ConditionalOnProperty(value = "invoicing-system.database.type", havingValue = "file")
 @Repository
 @RequiredArgsConstructor
 public class FileDatabase implements Database {
@@ -32,6 +34,7 @@ public class FileDatabase implements Database {
 
   @Override
   public Optional<Invoice> getById(String id) {
+    System.out.println("file database getById(id = " + id + ")");
     return getAll()
         .stream()
         .filter(invoice -> invoice.getId().equals(id))
--- app/src/main/java/pl/futurecollars/invoicing/db/InMemoryDatabase.java
+++ app/src/main/java/pl/futurecollars/invoicing/db/InMemoryDatabase.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import org.springframework.context.annotation.Primary;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Repository;
 import pl.futurecollars.invoicing.model.Invoice;
 
-@Primary
+@ConditionalOnProperty(value = "invoicing-system.database.type", havingValue = "inmemory")
 @Repository
 public class InMemoryDatabase implements Database {
 
@@ -25,6 +25,7 @@ public class InMemoryDatabase implements Database {
 
   @Override
   public Optional<Invoice> getById(String id) {
+    System.out.println("inMemory database getById(id = " + id + ")");
     return invoices.stream()
         .filter(invoice -> invoice.getId().equals(id))
         .findFirst();
