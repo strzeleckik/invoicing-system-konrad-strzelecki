9ded767b537b6edba122efcb329a1b1616080834
kstrzelecki <strzelecki@nonstop.co>
2023-02-27 19:58:15 +0100

    usused imports cleanup

--- app/src/main/java/pl/futurecollars/invoicing/db/FileDatabase.java
+++ app/src/main/java/pl/futurecollars/invoicing/db/FileDatabase.java
@@ -6,8 +6,6 @@ import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Repository;
 import pl.futurecollars.invoicing.configuration.InvoicingAppConfiguration;
